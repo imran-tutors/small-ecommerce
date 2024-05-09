622f2952ae09260605e6b1ca446a5206dae89fa8
imran-tutors <imran.hasan@tutors.es>
2024-05-09 19:09:37 +0600

    added customer nav and sidebar

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,3 +1,17 @@
+import { Outlet } from "react-router-dom";
+import CustomerNavbar from "../../Components/Customer/CustomerNavbar";
+import CustomerSidebar from "../../Components/Customer/CustomerSidebar";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <CustomerNavbar />
+      <div className="flex">
+        <CustomerSidebar />
+        <div className="bg-gray-100 min-h-[90vh] w-full p-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
