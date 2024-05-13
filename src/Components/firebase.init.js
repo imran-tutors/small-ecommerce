cdbbf0f0623ed2d5748a318c8e238b11545b899a
imran-tutors <imran.hasan@tutors.es>
2024-05-13 17:12:31 +0600

    added env

--- src/Components/firebase.init.js
+++ src/Components/firebase.init.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0ZldKeHc3fqLLLsFMK-W9qlXAMEBJQT8",
-  authDomain: "small-com.firebaseapp.com",
-  projectId: "small-com",
-  storageBucket: "small-com.appspot.com",
-  messagingSenderId: "336603040866",
-  appId: "1:336603040866:web:002860a9e69820de02d53b",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-
-export default auth
+export default auth;
