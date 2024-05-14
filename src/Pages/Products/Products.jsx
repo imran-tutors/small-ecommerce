cc2605470bc63712b7e28efef31cdf590f635291
imran-tutors <imran.hasan@tutors.es>
2024-05-14 18:04:59 +0600

    Design homepage

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,3 +1,14 @@
+import AllProducts from "../../Components/AllProducts/AllProducts";
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <div>
+      <div className="border bg-blue-100 px-20 py-24 rounded-xl">
+        <h1 className="text-4xl font-bold">Our products</h1>
+      </div>
+      <div className="py-8">
+        <AllProducts page="all-products" />
+      </div>
+    </div>
+  );
 }
