faad3c319ec42c5632788e279d05464729ecaef3
imran-tutors <mahmud.hasan@tutors.es>
2024-05-16 23:01:16 +0600

    added product detail page

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,11 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 // eslint-disable-next-line react/prop-types
 export default function ProductCard({ data }) {
   // eslint-disable-next-line react/prop-types
-  const { title, price, image } = data;
-  console.log(data);
+  const { _id, title, price, image } = data;
+  const navigate = useNavigate();
+
+  const handleNavigate = (id) => {
+    console.log(id);
+    navigate(`/products/${id}`);
+  };
+
   return (
     <div className="border rounded-lg">
-      <img className="w-full h-[280px] object-cover" src={image} alt="" />
+      <img
+        className="w-full h-[280px] object-cover"
+        src={image}
+        alt=""
+        onClick={() => handleNavigate(_id)}
+      />
       <div className="p-2">
         <p className="text-medium font-medium truncate  text-ellipsis">
           {title}
